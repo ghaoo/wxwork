ab8e41aadec83d8827d874627a6b69cf1b7a44b4
guhao022 <378999587@qq.com>
2020-01-08 18:11:26 +0800

    上传永久图片素材

--- media_upload.go
+++ media_upload.go
@@ -7,6 +7,7 @@ import (
 	"mime/multipart"
 	"net/url"
 	"os"
+	"path"
 	"path/filepath"
 	"strconv"
 )
@@ -100,4 +101,76 @@ func (a *Agent) MediaUpload(file string) (*Media, error) {
 	}
 
 	return a.UploadMediaWithType(mediaType, buf, info)
+}
+
+// UploadImgWithType 上传图片
+// 文档: https://work.weixin.qq.com/api/doc/90000/90135/90256
+func (a *Agent) UploadImgWithType(buf []byte, info os.FileInfo) (string, error) {
+	buffer := &bytes.Buffer{}
+	writer := multipart.NewWriter(buffer)
+
+	fw, err := writer.CreateFormFile("media", info.Name())
+	if err != nil {
+		return "", err
+	}
+
+	fw.Write(buf)
+
+	writer.WriteField("name", info.Name())
+	writer.WriteField("filename", info.Name())
+	writer.WriteField("filelength", strconv.FormatInt(info.Size(), 10))
+	writer.Close()
+
+	accessToken, err := a.GetAccessToken()
+	if err != nil {
+		return "", err
+	}
+
+	query := url.Values{}
+	query.Set("access_token", accessToken)
+
+	u, err := url.Parse(BaseURL)
+	if err != nil {
+		panic(err)
+	}
+
+	u.Path = path.Join(u.Path, "media/uploadimg")
+
+	u.RawQuery = query.Encode()
+
+	resp, err := a.client.Post(u.String(), writer.FormDataContentType(), buffer)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	var caller struct {
+		baseCaller
+		Url string
+	}
+
+	if err = json.NewDecoder(resp.Body).Decode(&caller); err != nil {
+		return "", err
+	}
+
+	if !caller.Success() {
+		return "", caller.Error()
+	}
+
+	return caller.Url, err
+}
+
+// UploadImg 上传图片
+func (a *Agent) UploadImg(file string) (string, error) {
+	info, err := os.Stat(file)
+	if err != nil {
+		return "", err
+	}
+
+	buf, err := ioutil.ReadFile(file)
+	if err != nil {
+		return "", err
+	}
+
+	return a.UploadImgWithType(buf, info)
 }
