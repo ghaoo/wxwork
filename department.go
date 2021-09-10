19ad2c4f323a6210e2381d2bafc8e981bab9c116
guhao022 <378999587@qq.com>
2021-09-10 15:27:17 +0800

    fix

--- department.go
+++ department.go
@@ -11,13 +11,13 @@ import (
 // 文档地址: https://work.weixin.qq.com/api/doc/90000/90135/90204
 type Department struct {
 	// 部门id，32位整型，指定时必须大于1。若不填该参数，将自动生成id
-	ID int `json:"id,omitempty" xml:"Id"`
+	ID int `json:"id,omitempty" xml:"Id,omitempty"`
 	// 部门名称。长度限制为1~32个字符，字符不能包括\:?”<>｜
-	Name string `json:"name" xml:"Name"`
+	Name string `json:"name,omitempty" xml:"Name,omitempty"`
 	// 英文名称，需要在管理后台开启多语言支持才能生效。长度限制为1~32个字符，字符不能包括\:?”<>｜
 	NameEn string `json:"name_en,omitempty" xml:"name_en,omitempty"`
 	// 父部门id，32位整型
-	ParentID int `json:"parentid" xml:"ParentID"`
+	ParentID int `json:"parentid,omitempty" xml:"ParentID,omitempty"`
 	// 在父部门中的次序值。order值大的排序靠前。有效的值范围是[0, 2^32)
 	Order int `json:"order,omitempty" xml:"order,omitempty"`
 }
