4035b684181b3eca59a465d31e90fa9c3b4f25cf
guhao022 <378999587@qq.com>
2021-09-24 10:14:21 +0800

    + 客户联系接口

--- external_follow_user.go
+++ external_follow_user.go
@@ -5,9 +5,9 @@ import (
 	"encoding/json"
 )
 
-// GetFollowUserList 获取配置了客户联系功能的成员列表
+// ExternalGetFollowUserList 获取配置了客户联系功能的成员列表
 // 文档: https://work.weixin.qq.com/api/doc/90000/90135/92571
-func (a *Agent) GetFollowUserList() ([]string, error) {
+func (a *Agent) ExternalGetFollowUserList() ([]string, error) {
 	var caller struct {
 		baseCaller
 		FollowUser []string `json:"follow_user"`
@@ -97,9 +97,9 @@ type Conclusions struct {
 	Miniprogram ConclusionsMiniprogram `json:"miniprogram,omitempty"`
 }
 
-// AddContactWay 配置客户联系「联系我」方式
+// ExternalAddContactWay 配置客户联系「联系我」方式
 // 文档: https://work.weixin.qq.com/api/doc/90000/90135/92572#%E9%85%8D%E7%BD%AE%E5%AE%A2%E6%88%B7%E8%81%94%E7%B3%BB%E3%80%8C%E8%81%94%E7%B3%BB%E6%88%91%E3%80%8D%E6%96%B9%E5%BC%8F
-func (a *Agent) AddContactWay(cw ContactWay) (map[string]string, error) {
+func (a *Agent) ExternalAddContactWay(cw ContactWay) (map[string]string, error) {
 	body, _ := json.Marshal(cw)
 
 	var caller struct {
@@ -122,9 +122,9 @@ func (a *Agent) AddContactWay(cw ContactWay) (map[string]string, error) {
 	return resp, nil
 }
 
-// GetContactWay 获取企业已配置的「联系我」方式
+// ExternalGetContactWay 获取企业已配置的「联系我」方式
 // 文档: https://work.weixin.qq.com/api/doc/90000/90135/92572#%E8%8E%B7%E5%8F%96%E4%BC%81%E4%B8%9A%E5%B7%B2%E9%85%8D%E7%BD%AE%E7%9A%84%E3%80%8C%E8%81%94%E7%B3%BB%E6%88%91%E3%80%8D%E6%96%B9%E5%BC%8F
-func (a *Agent) GetContactWay(config_id string) (ContactWay, error) {
+func (a *Agent) ExternalGetContactWay(config_id string) (ContactWay, error) {
 	param := map[string]string{
 		"config_id": config_id,
 	}
@@ -140,11 +140,11 @@ func (a *Agent) GetContactWay(config_id string) (ContactWay, error) {
 	return caller.ContactWay, err
 }
 
-// ListContactWay 获取企业已配置的「联系我」列表
+// ExternalListContactWay 获取企业已配置的「联系我」列表
 // 文档: https://work.weixin.qq.com/api/doc/90000/90135/92572#%E8%8E%B7%E5%8F%96%E4%BC%81%E4%B8%9A%E5%B7%B2%E9%85%8D%E7%BD%AE%E7%9A%84%E3%80%8C%E8%81%94%E7%B3%BB%E6%88%91%E3%80%8D%E5%88%97%E8%A1%A8
 // @response contact_way 联系方式的配置id
 // @response next_cursor 分页参数，用于查询下一个分页的数据，为空时表示没有更多的分页
-func (a *Agent) ListContactWay(start_time, end_time int64, limit int, cursor string) (contact_way []map[string]string, next_cursor string, err error) {
+func (a *Agent) ExternalListContactWay(start_time, end_time int64, limit int, cursor string) (contact_way []map[string]string, next_cursor string, err error) {
 	param := map[string]interface{}{
 		"start_time": start_time,
 		"end_time":   end_time,
@@ -164,9 +164,9 @@ func (a *Agent) ListContactWay(start_time, end_time int64, limit int, cursor str
 	return caller.ContactWay, caller.NextCursor, err
 }
 
-// UpdateContactWay 更新企业已配置的「联系我」方式
+// ExternalUpdateContactWay 更新企业已配置的「联系我」方式
 // 文档: https://work.weixin.qq.com/api/doc/90000/90135/92572#%E6%9B%B4%E6%96%B0%E4%BC%81%E4%B8%9A%E5%B7%B2%E9%85%8D%E7%BD%AE%E7%9A%84%E3%80%8C%E8%81%94%E7%B3%BB%E6%88%91%E3%80%8D%E6%96%B9%E5%BC%8F
-func (a *Agent) UpdateContactWay(cw ContactWay) error {
+func (a *Agent) ExternalUpdateContactWay(cw ContactWay) error {
 	body, _ := json.Marshal(cw)
 
 	var caller baseCaller
@@ -174,9 +174,9 @@ func (a *Agent) UpdateContactWay(cw ContactWay) error {
 	return a.ExecuteWithToken("POST", "externalcontact/update_contact_way", nil, bytes.NewReader(body), &caller)
 }
 
-// DelContactWay 删除企业已配置的「联系我」方式
+// ExternalDelContactWay 删除企业已配置的「联系我」方式
 // 文档: https://work.weixin.qq.com/api/doc/90000/90135/92572#%E5%88%A0%E9%99%A4%E4%BC%81%E4%B8%9A%E5%B7%B2%E9%85%8D%E7%BD%AE%E7%9A%84%E3%80%8C%E8%81%94%E7%B3%BB%E6%88%91%E3%80%8D%E6%96%B9%E5%BC%8F
-func (a *Agent) DelContactWay(config_id string) error {
+func (a *Agent) ExternalDelContactWay(config_id string) error {
 	param := map[string]string{
 		"config_id": config_id,
 	}
@@ -187,9 +187,9 @@ func (a *Agent) DelContactWay(config_id string) error {
 	return a.ExecuteWithToken("POST", "externalcontact/del_contact_way", nil, bytes.NewReader(body), &caller)
 }
 
-// CloseTempChat 结束临时会话
+// ExternalCloseTempChat 结束临时会话
 // 文档: https://work.weixin.qq.com/api/doc/90000/90135/92572#%E7%BB%93%E6%9D%9F%E4%B8%B4%E6%97%B6%E4%BC%9A%E8%AF%9D
-func (a *Agent) CloseTempChat(userid, external_userid string) error {
+func (a *Agent) ExternalCloseTempChat(userid, external_userid string) error {
 	param := map[string]string{
 		"userid":          userid,
 		"external_userid": external_userid,
